3d6ae31f3dd64a66298de883dc798effc1cec55c
Geoffrey Vancoetsem <geoff@geoffzone.net>
2018-10-17 01:15:22 +0200

    Impl: XSRF protection (angular side)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
@@ -20,7 +20,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    NgbModule.forRoot(),
     HttpClientModule,
+    HttpClientXsrfModule.withOptions({
+      headerName: 'X-Zeus-Lightning',
+      cookieName: 'Zeus-Lightning'
+    }),
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
