769e35f844b168ce0e849904c31a22d246972de0
Geoffrey Vancoetsem <geoff@geoffzone.net>
2018-12-09 18:24:25 +0100

    Angular: browse routing with single fso resolver

--- client/src/app/browse/browse-routing.module.ts
+++ client/src/app/browse/browse-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowseComponent } from './browse/browse.component';
+import { SingleFsoResolverService } from '../core/services/single-fso-resolver.service';
 
 const appRoutes: Routes = [
 	{
-		path: 'browse',
-		component: BrowseComponent
+		path: 'browse/:path',
+		component: BrowseComponent,
+		resolve: {
+			fsoResult: SingleFsoResolverService
+		}
 	}
 ];
 
